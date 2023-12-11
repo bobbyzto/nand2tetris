b833f529809e2d53c06118aa99020167e3ef3b51
Robert Weche <bobbyweche@gmail.com>
2023-12-11 19:52:51 +0000

    added associated emulator manuals and implemented Ch. 4 asm programs

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,26 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+  @R2
+  M=0	// R2=0
+  @i
+  M=0   // i=0
+(LOOP)
+  @i
+  D=M	// D register=i
+  @R0
+  D=D-M	// i=i-R0
+  @END
+  D;JGE // Jump to END if i<=0
+
+  @R1
+  D=M	// D register=R1
+  @R2	
+  M=D+M	// R2=R1+R2
+  @i
+  M=M+1	// increment index
+  @LOOP
+  0;JMP	// Jump to LOOP
+(END)	// infinite loop
+  @END
+  0,JMP
